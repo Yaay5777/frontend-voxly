0bedff2f0e413b50cc154d39ef720d7acea723eb
Yaay5777 <ihateu57055@gmail.com>
2025-09-27 18:11:23 +0300

    Add favicon link in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
-import { Toaster } from 'react-hot-toast';
-import '../styles/globals.css';
-
-// Dynamically import BrowserRouter to avoid SSR issues
-const BrowserRouter = dynamic(
-  () => import('react-router-dom').then((mod) => mod.BrowserRouter),
-  { ssr: false }
-);
+import Head from "next/head";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <BrowserRouter>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
-      <Toaster position="top-right" />
-    </BrowserRouter>
+    </>
   );
 }
 
